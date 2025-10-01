f9d315035eda72f1af7d37ee4ced30e6a4b97ad6
Martín Portela <martinportelasegui@gmail.com>
2025-10-01 10:55:00 +0200

    Formularios

---
+++ forms/calculadora.php
@@ -0,0 +1,9 @@
+<?php
+echo "El method que ha usado es: ",$_SERVER['REQUEST_METHOD'],"<br>";
+echo  $_POST['operando1'],"<br>";
+echo  $_POST['operando2'],"<br>";
+echo  $_POST['suma'],"<br>";
+echo  $_POST['resta'],"<br>";
+echo  $_POST['producto'],"<br>";
+echo  $_POST['division'],"<br>";
+?>
